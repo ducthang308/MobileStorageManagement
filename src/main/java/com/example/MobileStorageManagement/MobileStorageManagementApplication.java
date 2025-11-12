812a2718ac0f2bbcd04d300e7abb185e721178b3
lamhai <hoangdinhlamhai1@gmail.com>
2025-11-12 17:08:21 +0700

    init commit

---
+++ src/main/java/com/example/MobileStorageManagement/MobileStorageManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.MobileStorageManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileStorageManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileStorageManagementApplication.class, args);
+	}
+
+}
